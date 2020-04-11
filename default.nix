64de462cf93a408f3494780ba253ad7c6124e076
codygman <cody@codygman.dev>
2020-04-11 18:16:48 -0500

    add default.nix; remove stack.yaml

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ compilerVersion ? "ghc883" ,
+  pkgs ?
+  import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz";
+    sha256 = "1gqv2m7plkladd3va664xyqb962pqs4pizzibvkm1nh0f4rfpxvy";
+  }) {}}:
+let
+  compiler = pkgs.haskell.packages."${compilerVersion}";
+  pkg = compiler.developPackage
+    { root = ./.;
+      # overrides = self: super: {};
+      # source-overrides = {};
+    };
+in pkg
